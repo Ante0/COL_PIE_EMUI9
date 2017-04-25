ebfd3f82446dcb4053349e2b82a071531e64f324
Teng Qin <qinteng@fb.com>
2017-04-24 19:00:37 -0700

    bpf: map_get_next_key to return first key on NULL
    commit 8fe45924387be6b5c1be59a7eb330790c61d5d10 upstream.
    
    When iterating through a map, we need to find a key that does not exist
    in the map so map_get_next_key will give us the first key of the map.
    This often requires a lot of guessing in production systems.
    
    This patch makes map_get_next_key return the first key when the key
    pointer in the parameter is NULL.
    
    Signed-off-by: Teng Qin <qinteng@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Cc: Lorenzo Colitti <lorenzo@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -574,11 +574,6 @@ static int map_get_next_key(union bpf_attr *attr)
 	if (IS_ERR(map))
 		return PTR_ERR(map);
 
-	if (!(f.file->f_mode & FMODE_CAN_READ)) {
-		err = -EPERM;
-		goto err_put;
-	}
-
 	if (ukey) {
 		err = -ENOMEM;
 		key = kmalloc(map->key_size, GFP_USER);
