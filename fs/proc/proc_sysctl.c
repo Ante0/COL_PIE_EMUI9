7ec530cc969a651829d32898dfb0a38e52cb07d1
ante0 <ante0@ubuntu.ubuntu-domain>
2018-12-20 21:39:28 +0100

    typo

--- fs/proc/proc_sysctl.c
+++ fs/proc/proc_sysctl.c
@@ -486,7 +486,7 @@ out:
 void proc_sys_evict_inode(struct inode *inode, struct ctl_table_header *head)
 {
 	spin_lock(&sysctl_lock);
-	list_del(&PROC_I(inode)->sysctl_inodes)
+	list_del(&PROC_I(inode)->sysctl_inodes);
 	if (!--head->count)
 		kfree_rcu(head, rcu);
 	spin_unlock(&sysctl_lock);
